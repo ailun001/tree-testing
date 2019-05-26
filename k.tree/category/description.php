4001c9a61bedfc388cdfd103a2ce2b25d3866656
ailun001 <43659626+ailun001@users.noreply.github.com>
2019-05-26 15:56:43 +1200

    Update description.php

--- k.tree/category/description.php
+++ k.tree/category/description.php
@@ -1,5 +1,5 @@
 
-<?php include './../../header.php'?>
+<?php include './../header.php'?>
 <div class="content">
     
     <?php
@@ -30,5 +30,5 @@
     ?>
     
 </div>
-<?php include './../../footer.php'?>
+<?php include './../footer.php'?>
 
